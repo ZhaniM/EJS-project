8df023d6ec4d677d1d4df3960b35deaf7a076128
ZhaniM <zhani.muceku@cit.edu.al>
2020-11-04 12:46:00 +0100

    add gitignore

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
+let emer="lol"
 
-
-let app=express();
+const app=express();
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-res.send("prove")
+res.render('home',{prove:emer});
 })
 app.listen(3000,()=>{
     
